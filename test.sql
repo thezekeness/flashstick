1f2f94cfe81dee11c7040ca648fc8485f929c34c
TheZekeness <zekemonny@gmail.com>
2013-03-25 18:03:17 -0700

    Initial commitment of files.

---
+++ test.sql
@@ -0,0 +1,10 @@
+CREATE TABLE testtable (
+	id INTEGER PRIMARY KEY,
+	fname TEXT,
+	lname TEXT);
+
+INSERT INTO testtable (id, fname, lname) VALUES 
+	(001, "Zeker", "Zuniga"),
+	(002, "Lucero", "Zuniga"),
+	(003, "George", "Moreno"),
+	(004, "Angie", "Moreno");
